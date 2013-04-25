3cb8724c97291d6d3a3f3027efaef9b143ba59cc
Alan Wijntje <alan.wijntje@gmail.com>
2013-04-25 07:41:24 +0200

    added basic code for capturing of traps.

---
+++ mac-trap.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl -w
+
+use strict;
+use lib "/usr/local/nagios/perl/lib";
+use SNMP::Trapinfo;
+use Data::Dumper;
+
+my $trap;
+my $timestamp = localtime(time);
+
+open (LOG,">>/tmp/mac-trap.log") or die "Can't open logfile: $!\n";
+print LOG "$timestamp recieved trap.\n";
+while ($trap = SNMP::Trapinfo->new(*STDIN)) {
+	print LOG $trap->packet."\n";
+}
+close LOG;
+
